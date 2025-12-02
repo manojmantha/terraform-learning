69b4e4a7d7e46a61bebf82555bf49cc81e3d2548
manoj515876 <manojputtaluckey@gmail.com>
2025-12-02 08:39:18 +0530

    Creating EC2 Instance using Terraform

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instace" "this" {
+resource "aws_instance" "this" {
   ami = "ami-09c813fb71547fc4f"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   instance_type = "t3.micro"
